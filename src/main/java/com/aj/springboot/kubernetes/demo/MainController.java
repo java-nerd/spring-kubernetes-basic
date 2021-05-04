bcfc81decddce2471e3d08a0016c653c8fd13e3e
ajay74984 <51405689+ajay74984@users.noreply.github.com>
2021-05-04 08:43:20 +0200

    multiple deployment using postgres

--- src/main/java/com/aj/springboot/kubernetes/demo/MainController.java
+++ src/main/java/com/aj/springboot/kubernetes/demo/MainController.java
@@ -4,6 +4,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 //we are using Thymeleaf, no need for RestController
 @Controller
 public class MainController {
@@ -13,5 +16,18 @@ public class MainController {
 		model.addAttribute("fname", "Ajay");
 		model.addAttribute("lname", "Garg");
 		return "index";
+	}
+}
+
+class Test {
+	public static void main(String[] args) {
+		List<String> v1 = new ArrayList<>();
+		v1.add("1");
+		v1.add("2");
+		v1.add("3");
+		List<String> v2 = new ArrayList<>();
+		v2.add("2");
+		final boolean b = v1.removeAll(v2);
+		System.out.println(v1);
 	}
 }
